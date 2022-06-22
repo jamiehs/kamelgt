fb04af7c3ec35c58e250cca3c0054b614ad5c01d
Jamie Hamel-Smith <jamie3d@gmail.com>
2022-06-21 21:18:44 -0700

    Makes broadcasts prettier

--- src/components/Broadcast/Broadcast.js
+++ src/components/Broadcast/Broadcast.js
@@ -11,6 +11,10 @@ function Broadcast(props) {
         return matches[1]
     }
 
+    function randomGlitchId(variants = 5) {
+        return Math.ceil(Math.random() * variants)
+    }
+
     return (
         <div className="Broadcast">
             <a className="no-icon" href={url} target="_blank" rel="noreferrer">
@@ -18,9 +22,15 @@ function Broadcast(props) {
             </a>
                 <div className="yt-video">
             <a className="no-icon" href={url} target="_blank" rel="noreferrer">
-                    <div className="thumb" style={{backgroundImage: `url(https://img.youtube.com/vi/${getId(url)}/1.jpg)`}}></div>
-                    <div className="thumb" style={{backgroundImage: `url(https://img.youtube.com/vi/${getId(url)}/2.jpg)`}}></div>
-                    <div className="thumb" style={{backgroundImage: `url(https://img.youtube.com/vi/${getId(url)}/3.jpg)`}}></div>
+                    <div className="thumb" style={{backgroundImage: `url(https://img.youtube.com/vi/${getId(url)}/1.jpg)`}}>
+                        <div className={`glitch glitch-${randomGlitchId()}`}></div>
+                    </div>
+                    <div className="thumb" style={{backgroundImage: `url(https://img.youtube.com/vi/${getId(url)}/2.jpg)`}}>
+                        <div className={`glitch glitch-${randomGlitchId()}`}></div>
+                    </div>
+                    <div className="thumb" style={{backgroundImage: `url(https://img.youtube.com/vi/${getId(url)}/3.jpg)`}}>
+                        <div className={`glitch glitch-${randomGlitchId()}`}></div>
+                    </div>
             </a>
                 </div>
         </div>
