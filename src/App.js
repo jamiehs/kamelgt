d341fe9952a7689c9512bafb59d420ee79fe375c
Jamie Hamel-Smith <jamie3d@gmail.com>
2022-07-02 15:12:34 -0700

    Adds new setups for Brands Hatch & Sebring

--- src/App.js
+++ src/App.js
@@ -110,6 +110,10 @@ function App() {
             <div className="broadcast section">
                 <h2 className="title">Previous Broadcast Races</h2>
                 <Broadcast
+                    title="2022 Season 3 Round 3: Watkins Glen"
+                    url="https://youtu.be/bncdRkxyUZI?t=1208"
+                />
+                <Broadcast
                     title="2022 Season 3 Round 2: Fuji"
                     url="https://youtu.be/5huzNPgmsJc?t=1043"
                 />
