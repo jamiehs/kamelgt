17abc9252090d4786d00ad7f1b74e0c54f812b7e
Jamie Hamel-Smith <jamie3d@gmail.com>
2023-03-14 22:05:50 -0700

    Moves to using state for the glitch ID

--- src/components/Broadcast/Broadcast.js
+++ src/components/Broadcast/Broadcast.js
@@ -1,34 +1,49 @@
 import './Broadcast.scss';
+import React from 'react';
 
-function Broadcast(props) {
-    let {
-        title,
-        url,
-    } = props
+class Broadcast extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            glitchId: 0
+        }
+    }
+    componentDidMount() {
+        this.setState({
+            glitchId: this.randomGlitchId()
+        })
+    }
 
-    function getId(url) {
+    getId(url) {
         let matches = url.match(/.*(?:youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#&?]*).*/)
         return matches[1]
     }
 
-    function randomGlitchId(variants = 5) {
+    randomGlitchId(variants = 5) {
         return Math.ceil(Math.random() * variants)
     }
 
-    return (
-        <div className="Broadcast">
-            <a href={url} target="_blank" rel="noreferrer">
-                <h3>{title}</h3>
-            </a>
-                <div className="yt-video">
-            <a className="no-icon" href={url} target="_blank" rel="noreferrer">
-                <div className="thumb" style={{backgroundImage: `url(https://img.youtube.com/vi/${getId(url)}/3.jpg)`}}>
-                    <div className={`glitch glitch-${randomGlitchId()}`}></div>
-                </div>
-            </a>
-                </div>
-        </div>
-    );
+    render() {
+        let {
+            title,
+            url,
+        } = this.props
+
+        return (
+            <div className="Broadcast">
+                <a href={url} target="_blank" rel="noreferrer">
+                    <h3>{title}</h3>
+                </a>
+                    <div className="yt-video">
+                <a className="no-icon" href={url} target="_blank" rel="noreferrer">
+                    <div className="thumb" style={{backgroundImage: `url(https://img.youtube.com/vi/${this.getId(url)}/3.jpg)`}}>
+                        <div className={`glitch glitch-${this.state.glitchId}`}></div>
+                    </div>
+                </a>
+                    </div>
+            </div>
+        );
+    }
 }
 
 export default Broadcast;
