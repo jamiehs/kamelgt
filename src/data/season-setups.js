e4208d1d638bd3c311c22277b4b688d69e9aa43a
Jamie Hamel-Smith <jamie3d@gmail.com>
2023-12-09 14:40:27 -0800

    Adds setups for 24S1 Imola and Nurburgring GP

--- src/data/season-setups.js
+++ src/data/season-setups.js
@@ -97,16 +97,52 @@ const seasonSetups = [
         ...tracks.IMOLA,
         "weekStart": "2024-01-16",
         "audi90gto": [
+            {
+                "file": "maf_imola_23s4_r1b.sto"
+            },
+            {
+                "file": "imola-23S4-r1.sto"
+            },
+            {
+                "file": "A90_23S4_Imola_Y_Gijsen_R.sto"
+            },
+            {
+                "file": "A90_23S4_Imola_Y_Gijsen_Q.sto"
+            },
+            {
+                "file": "andrius_sl_imola_1_0_R.sto"
+            },
         ],
         "nissangtpzxt": [
+            {
+                "file": "Lgo22S4_MH_Imola_R_v1.3_R62.sto"
+            },
+            {
+                "file": "Lgo22S4_MH_Imola_R_v1.3_Q61.sto"
+            },
+            {
+                "file": "GTP_Imola_JdelOlmoR.sto"
+            },
         ]
     },
     {
         ...tracks.NURBURGRING_GP_BES_WEC,
         "weekStart": "2024-01-23",
         "audi90gto": [
+            {
+                "file": "KB_nurburgring_gp_23_v1.sto"
+            },
+            {
+                "file": "DWC_22S3_nurburgring_gp_StreckeArena_R2.sto"
+            },
         ],
         "nissangtpzxt": [
+            {
+                "file": "asg-nurbgp-r.sto"
+            },
+            {
+                "file": "BL_Nurburgring_GP_6-2_R.sto"
+            },
         ]
     },
     {
